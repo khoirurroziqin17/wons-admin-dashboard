64350078543af0c6cd8bfa8d37ff5a442c80cd16
khoirurroziqin17 <khoirurroziqin951@gmail.com>
2023-06-04 11:59:00 +0700

    deleted flowbite

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,jsx}",
-    "node_modules/flowbite/**/*.{js,jsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,jsx}"],
   theme: {
     extend: {},
   },
-  plugins: [require("flowbite/plugin")],
+  plugins: [],
 };
